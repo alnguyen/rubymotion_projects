60785eb79f00435b2f860a0984f7cfbfad93003b
Alan Nguyen <nguyen.alan@me.com>
2012-11-26 19:45:18 -0700

    add provisioning profile.  animated a square

--- Hello/app/app_delegate.rb
+++ Hello/app/app_delegate.rb
@@ -5,14 +5,17 @@ class AppDelegate
 
     controller = TapController.alloc.initWithNibName(nil, bundle: nil)
   	nav_controller = UINavigationController.alloc.initWithRootViewController(controller)
+  	nav_controller.title = "Taps"
   	alphabet_controller = AlphabetController.alloc.initWithNibName(nil, bundle: nil)
+  	animate_controller = AnimateController.alloc.initWithNibName(nil, bundle: nil)
+  	animate_controller.title = "Animate"
 
-		# other_controller = UIViewController.alloc.initWithNibName(nil, bundle: nil)
-		# other_controller.title = "Other"
-		# other_controller.view.backgroundColor = UIColor.purpleColor
+		other_controller = UIViewController.alloc.initWithNibName(nil, bundle: nil)
+		other_controller.title = "Other"
+		other_controller.view.backgroundColor = UIColor.purpleColor
 
    	tab_controller = UITabBarController.alloc.initWithNibName(nil, bundle: nil)
-  	tab_controller.viewControllers = [alphabet_controller, nav_controller]
+  	tab_controller.viewControllers = [alphabet_controller, other_controller, animate_controller, nav_controller]
   	@window.rootViewController = tab_controller
     true
   end
